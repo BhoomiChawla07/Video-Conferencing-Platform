7824be63eeb270aff8f3a7f83bdee140ed1ce18b
BhoomiChawla07 <bhoomichawla07@gmail.com>
2026-05-23 21:54:18 +0530

    fixed deployment issue

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,8 @@
-const server = "http://localhost:8000";
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://video-conferencing-backend-mvi4.onrender.com" :
+
+    "http://localhost:8000"
+
 
 export default server;
